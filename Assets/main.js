37a10415f435373bf731ebdb1818b80681b602f6
HIRWA Jean Eric <hirwaminerve25@gmail.com>
2022-04-05 10:30:53 +0200

    Update main.js

--- Assets/main.js
+++ Assets/main.js
@@ -1,62 +1,56 @@
-const saveBtn = document.getElementById('save');
+const saveBtn = document.getElementById("save");
 let taskDiv = "";
 let name = "";
 let date = "";
 let completed = "";
 let taskContainer = "";
 
-saveBtn.addEventListener('click',addTask);
+saveBtn.addEventListener("click", addTask);
 
 function addTask() {
-    //Getting data form the form
-    name = document.getElementById('name').value;
-    date = document.getElementById('date').value;
-    completed = document.getElementsByName('completed');
+    name = document.getElementById("name").value;
+    date = document.getElementById("date").value;
+    completed = document.getElementById("yes");
 
-    if (name== "" || date == "" || date=="") {
+  if (name == "" || date == "") {
         alert("Please, first make sure all inputs are filled!");
-    } else {     
-        //get task container
-        taskContainer = document.getElementById('task-container');
+  } else {
+        taskContainer = document.getElementById("task-container");
 
-        if (completed[0].checked) {
-            createColloredTask("bg-green-600");
-            putTasksInDiv("line-through");      
-            clearFields();
-        } else {
-            createColloredTask("bg-slate-300");
-            putTasksInDiv("block");
-            clearFields();
-        }
+    if (completed.checked) {
+        createColoredTaskDiv("bg-green-600");
+        putTasksInDiv("line-through");
+        clearFields();
+    } else {
+        createColoredTaskDiv("bg-slate-300");
+        putTasksInDiv("block");
+        clearFields();
+    }
+  }
 }
 
-function createColloredTask(backgroundColor) {
-    //create a new div to append to the container
-    taskDiv = document.createElement('div');
-    taskDiv.classList.add("mb-3","flex" ,"flex-col" ,"w-100%" ,"shadow-md" ,"border" ,"rounded-lg" ,"border-transparent" ,"shadow-lg" ,"shadow-black","m-2","hover:text-gray-600");
-    taskDiv.classList.add(`${backgroundColor}`);                 
-    }
+function createColoredTaskDiv(backgroundColor) {
+    taskDiv = document.createElement("div");
+    taskDiv.classList.add("mb-3","flex","flex-col","w-100%","shadow-md","border","rounded-lg","border-transparent","shadow-lg","shadow-black","m-2","hover:text-gray-600");
+    taskDiv.classList.add(`${backgroundColor}`);
 }
 
 function putTasksInDiv(linethroughStyle) {
-    //Create paragraph elements to go inside the task div
-    const taskName = document.createElement('p');
-    taskName.classList.add("mt-2" ,"ml-2" ,"mr-2");
+    const taskName = document.createElement("p");
+    taskName.classList.add("mt-2", "ml-2", "mr-2");
     taskName.classList.add(`${linethroughStyle}`);
     taskName.innerHTML = name;
 
-    const taskDate = document.createElement('p');
-    taskDate.classList.add("mt-2" ,"ml-2" ,"mb-2" ,"ml-3" ,"mr-3" ,"text-xs" ,"text-right");
+    const taskDate = document.createElement("p");
+    taskDate.classList.add("mt-2","ml-2","mb-2","ml-3","mr-3","text-xs","text-right");
     taskDate.innerHTML = date;
-    
-    //Adding the paragraph elements to the div
+
     taskDiv.appendChild(taskName);
     taskDiv.appendChild(taskDate);
-    //Adding the task div to the task container
-    taskContainer.appendChild(taskDiv)        
+    taskContainer.appendChild(taskDiv);
 }
 
 function clearFields() {
-    document.getElementById('name').value = '';
-    document.getElementById('date').value = '';
+    document.getElementById("name").value = "";
+    document.getElementById("date").value = "";
 }
